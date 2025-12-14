fdd1a7609620f7972712edaa85055469073885d1
Tataru Tudor Sorin <rodut11@protonmail.com>
2025-12-14 12:37:14 +0200

    added instructions and operators

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <stdint.h>
 
-int main(void) {
-    printf("Hello, World!\n");
-    return 0;
-}
+typedef enum {
+    OP_END = 0,
+    OP_RIGHT_DP, // move pointer right | (>)
+    OP_LEFT_DP,  // move pointer left  | (<)
+    OP_INC_VAL,  // increase value     | (+)
+    OP_DEC_VAL,  // decrease value     | (-)
+    OP_OUT,      // output value       | (.)
+    OP_IN,       // input value        | (,)
+    OP_JMP_FWD,  // jump forward       | ([)
+    OP_JMP_REV,  // jump backwards     | (])
+
+}Operator;
+
+typedef struct {
+    Operator operator;
+    uint16_t operand;
+} Instruction;
+
+
+
